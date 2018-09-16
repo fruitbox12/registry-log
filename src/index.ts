349a94fe709efc037d6a6f178ba42acf9010f0fb
pheonix344 <pheonix34@protonmail.com>
2018-09-16 15:44:51 +0200

    outsource hypercore types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import "@netrunner/types";
 import { Entry, EntrySchema } from './pbf/Entry';
-import hypercore from "hypercore";
+import hypercore, { HypercoreOptions, Hypercore, HypercoreValueEncoding } from "hypercore";
 
 export function registryLog(storage: any,
-    key?: Buffer | null | HypercoreOptions<EntrySchema>,
+    key?: Buffer | Uint8Array | HypercoreOptions<EntrySchema>,
     opts?: HypercoreOptions<EntrySchema>): Hypercore<EntrySchema> {
     if (key && !(key instanceof Uint8Array)) {
         opts = key as HypercoreOptions<EntrySchema>;
