1755132813bfac4715320eb8f07bb282f7fb7ae0
pheonix344 <pheonix34@protonmail.com>
2018-09-22 13:44:42 +0200

    fix protobuf file

--- src/pbf/Entry.ts
+++ src/pbf/Entry.ts
@@ -4,10 +4,10 @@ export interface EntrySchema {
     key: Uint8Array;
     sig: Uint8Array;
     created: number;
-    updated: number;
-    removed: boolean;
+    updated?: number;
+    removed?: boolean;
     name: string;
-    target: string;
+    content: string;
 }
 
 export const Entry = {
@@ -20,8 +20,8 @@ export const Entry = {
             else if (tag === 4 && obj && pbf) { obj.updated = pbf.readVarint(); }
             else if (tag === 5 && obj && pbf) { obj.removed = pbf.readBoolean(); }
             else if (tag === 6 && obj && pbf) { obj.name = pbf.readString(); }
-            else if (tag === 6 && obj && pbf) { obj.target = pbf.readString(); }
-        }, { key: new Uint8Array(0), sig: new Uint8Array(0), created: 0, updated: 0, removed: false, name: "", target: "" });
+            else if (tag === 7 && obj && pbf) { obj.content = pbf.readString(); }
+        }, { key: new Uint8Array(0), sig: new Uint8Array(0), created: 0, updated: 0, removed: false, name: "", content: "" });
     },
 
     encode(obj: EntrySchema): Uint8Array {
@@ -32,7 +32,7 @@ export const Entry = {
         if (obj.updated) { pbf.writeVarintField(4, obj.updated); }
         if (obj.removed) { pbf.writeBooleanField(5, obj.removed); }
         if (obj.name) { pbf.writeStringField(6, obj.name); }
-        if (obj.target) { pbf.writeStringField(6, obj.target); }
+        if (obj.content) { pbf.writeStringField(7, obj.content); }
         const buffer = pbf.finish();
         return Buffer.from(buffer);
     }
