86a2121062fc6ffafa8a342e0167635624218616
pheonix344 <pheonix34@protonmail.com>
2018-10-03 15:29:11 +0200

    remove lib files from repository

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="@types/node" />
-import { HypercoreOptions, Hypercore } from "hypercore";
-import { EntrySchema } from "./pbf/Entry";
-export { EntrySchema, Entry } from './pbf/Entry';
-export * from "./util";
-export * from "./EntryWriter";
-export declare function registryLog(storage: any, key?: Buffer | Uint8Array | HypercoreOptions<EntrySchema>, opts?: HypercoreOptions<EntrySchema>): Hypercore<EntrySchema>;
-//# sourceMappingURL=index.d.ts.map
