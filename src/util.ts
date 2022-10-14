c1a6c186164839fc97d18cae11b392c40925c396
Dylan <dylanwong007@gmail.com>
2022-10-13 21:40:24 -0400

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,19 @@
 import { EntrySchema } from "./pbf/Entry";
 import { createHash } from "crypto";
 import { sign as sodiumSign, verify as sodiumVerify, SodiumSignaturesKeyPair } from "sodium-signatures";
+import { generate as stronglinkGen, verify as stronglinkVerify } from {'hypercore-strong-link'}
+const loglink = (message:string) => console.log('signed ' + link);
 
+interface StrongLinkError{
+    (message:string):void;
+}
+(err, link) {
+       if (err) throw err
+       console.log(link) // {feed: someFeed.key, seq: 42, treeHash: <buf>}
+       stronglinkVerify(entry.key, link, function (err, data) {
+       if (err) throw err
+    // otherwise the data at the seq is returned
+       console.log(data)
 export interface ShortEntrySchema {
     name: string;
     target: string;
@@ -32,12 +44,18 @@ export function createEntry(keys: SodiumSignaturesKeyPair, name: string, target:
     const entry: EntrySchema = {
         key: keys.publicKey,
         sig: Buffer.alloc(0),
+        verify: false,
         created: 0,
+        link: 0,
         updated: 0,
         name, target,
         removed: false
     };
     entry.sig = sign(keys.secretKey, entry);
+    entry.gen = stronglinkGen(entry.key, entry.updated, callback: StrongLinkError)
+    calback(entry.key)
+    entry.verify=stronglinkVerify(entry.key, entry.updated, callback: StrongLinkError)
+    
     return entry;
 }
 
